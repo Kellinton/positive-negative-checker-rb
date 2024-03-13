170117df9bdd59165b6e0b52a8d245f2b8ad9c2c
Kellinton <kellintonbrasil@gmail.com>
2024-03-13 07:09:40 -0300

    verificar numero

--- src/main.rb
+++ src/main.rb
@@ -1,1 +1,17 @@
 # verificar se um número é positivo ou negativo
+
+
+def check(numero)
+    if numero >= 0
+        return "#{numero} é Positivo"
+    else
+        return "#{numero} é Negativo"
+    end
+end
+
+def main()
+    resultado = check(2)
+    puts resultado
+end
+
+main()
